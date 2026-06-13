0caf49ed728a62ff897ec3fb823740392d4aef20
Adil Rahman <flutterdevelopmemt@gmail.com>
2026-06-14 00:06:11 +0530

    some changes in code

--- lib/core/supabase_config.dart
+++ lib/core/supabase_config.dart
@@ -1,8 +1,10 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class SupabaseConfig {
-  static const String supabaseUrl = 'https://emizngxsxlrbxsmkzstp.supabase.co';
-  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtaXpuZ3hzeGxyYnhzbWt6c3RwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5MDMxODgsImV4cCI6MjA4ODQ3OTE4OH0.o86dqkrAvLPZB-2CZwRu2z6p-Qq0O8UZGtYgouf6bqE';
-  static const String authCallbackUrl = 'https://emizngxsxlrbxsmkzstp.supabase.co/auth/v1/callback';
+  static String get supabaseUrl => dotenv.env['SUPABASE_URL'] ?? '';
+  static String get supabaseAnonKey => dotenv.env['SUPABASE_ANON_KEY'] ?? '';
+  static String get authCallbackUrl => '${supabaseUrl}/auth/v1/callback';
   
   // NOTE: This is required for Web/iOS native sign-in logic
-  static const String googleWebClientId = '1084175072519-6ki7im9dfmfooobm9h4n45anmflirg0d.apps.googleusercontent.com';
+  static String get googleWebClientId => dotenv.env['GOOGLE_WEB_CLIENT_ID'] ?? '';
 }
